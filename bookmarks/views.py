b0718a1e7875b4512b6bb6aea6ecce6783d9f355
Lucas Westine <westine@gmail.com>
2012-06-26 17:10:59 +0800

    First django

---
+++ bookmarks/views.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.utils.importlib import import_module
+from django.template import Context
+from django.template.loader import get_template
+
+def main_page(request):
+	template = get_template('main_page.html')
+	variables = Context({
+	  'head_title': 'Django Bookmarks',
+	  'page_title': 'Welcome to Django Bookmarks',
+	  'page_body': 'Where you can store and share bookmarks!'
+	})
+	output = template.render(variables)
+	return HttpResponse(output)
+
+def user_page(request, username):
+    try:
+		user = User.objects.get(username=username)
+	except:
+		raise Http404('Requested user not found.')
+	bookmarks = user.bookmark_set.all()
+	template = get_template('user_page.html')
+	variables = Context({
+			'username': username,
+			'bookmarks': bookmarks
+			})
+	output = template.render(variables)
+	return HttpResponse(output)
--- urls.py
+++ urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls.defaults import *
+from bookmarks.views import *
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
+    (r'^$', main_page),
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
+    ('^evertodo$', 'django.views.generic.simple.direct_to_template',
+     {'template': 'speedtest.html'}),
 )
+
