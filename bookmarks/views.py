64c83db15a5f8394a5ca1d63de2dda958952b6d4
Lucas Westine <westine@gmail.com>
2012-06-29 16:14:23 +0800

    this is the python27 version of django
    all next level app should base on this one

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -15,15 +15,16 @@ def main_page(request):
 	return HttpResponse(output)
 
 def user_page(request, username):
-    try:
+	try:
 		user = User.objects.get(username=username)
+        
 	except:
 		raise Http404('Requested user not found.')
 	bookmarks = user.bookmark_set.all()
 	template = get_template('user_page.html')
 	variables = Context({
-			'username': username,
-			'bookmarks': bookmarks
-			})
+		'username': username,
+		'bookmarks': bookmarks
+		})
 	output = template.render(variables)
 	return HttpResponse(output)
